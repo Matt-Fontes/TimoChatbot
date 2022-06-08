0617b3d57fceacd99e5f71677e9a5bb10d1e76cc
Matheus Fontes <49371318+Matt-Fontes@users.noreply.github.com>
2022-06-08 19:47:37 -0300

    Update actions

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,13 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+import json
+f = open('../data.json')
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +28,19 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionIngredientes(Action):
+
+    def name(self) -> Text:
+        return "action_ingredientes"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        data = json.load(f)
+        str = ''.join(data['prato'][tracker.get_slot('pedido')]['ingredientes'])
+
+        dispatcher.utter_message(text=str)
+
+        return []
